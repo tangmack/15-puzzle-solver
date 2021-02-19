aeb5659d5f6582061dffe17888c7caf05ef05c40
tangmack <macktang@gmail.com>
2021-02-19 16:49:34 -0700

    minor changes, addded timing

--- experiment_15puzzle.py
+++ experiment_15puzzle.py
@@ -88,236 +88,239 @@ class DualWayMovesDict():
             # elif value == 'D':
             #     self.dual_moves_dict[(i2,i1)] = 'U'
 
+def main():
+    # declare half of the mappings from tuple of block indexes (start, end) ie. (2,3) or (3,2)
+    # to respective actions ie. Right or Left
+    # Later we will pass this into a DualWayMovesDict() class and essentially "double" the dictionary
+    # since we know left and right, or up and down, are opposite moves to each other.
+    numbers_tuple_dict = {(0,1):'R',
+                          (1,2):'R',
+                          (2,3):'R',
+                          (4,5):'R',
+                          (5,6):'R',
+                          (6,7):'R',
+                          (8,9):'R',
+                          (9,10):'R',
+                          (10,11):'R',
+                          (12,13):'R',
+                          (13,14):'R',
+                          (14,15):'R',
+                          (0,4):'D',
+                          (1,5):'D',
+                          (2,6):'D',
+                          (3,7):'D',
+                          (4,8):'D',
+                          (5,9):'D',
+                          (6,10):'D',
+                          (7,11):'D',
+                          (8,12):'D',
+                          (9,13):'D',
+                          (10,14):'D',
+                          (11,15):'D',
+                          }
 
-# declare half of the mappings from tuple of block indexes (start, end) ie. (2,3) or (3,2)
-# to respective actions ie. Right or Left
-# Later we will pass this into a DualWayMovesDict() class and essentially "double" the dictionary
-# since we know left and right, or up and down, are opposite moves to each other.
-numbers_tuple_dict = {(0,1):'R',
-                      (1,2):'R',
-                      (2,3):'R',
-                      (4,5):'R',
-                      (5,6):'R',
-                      (6,7):'R',
-                      (8,9):'R',
-                      (9,10):'R',
-                      (10,11):'R',
-                      (12,13):'R',
-                      (13,14):'R',
-                      (14,15):'R',
-                      (0,4):'D',
-                      (1,5):'D',
-                      (2,6):'D',
-                      (3,7):'D',
-                      (4,8):'D',
-                      (5,9):'D',
-                      (6,10):'D',
-                      (7,11):'D',
-                      (8,12):'D',
-                      (9,13):'D',
-                      (10,14):'D',
-                      (11,15):'D',
-                      }
+    position_to_actionset_dict = DualWayMovesDict(numbers_tuple_dict)  # "double" the dictionary from positions to actions
 
-position_to_actionset_dict = DualWayMovesDict(numbers_tuple_dict)  # "double" the dictionary from positions to actions
+    # s = "abcdefghijklmnoX" # o is fifteen
+    # s = "abcdefghijklmnXo" # o is fifteen
+    # s = "Xabcdefghijklmno" # o is fifteen
+    # s = "abcdeXfghijklmno" # o is fifteen
 
-# s = "abcdefghijklmnoX" # o is fifteen
-# s = "abcdefghijklmnXo" # o is fifteen
-# s = "Xabcdefghijklmno" # o is fifteen
-# s = "abcdeXfghijklmno" # o is fifteen
+    # s = translate('01 02 03 04 05 06 08 14 09 11 07 00 13 10 15 12')
+    # s = translate('01 02 03 04 05 06 07 08 09 10 11 12 13 00 14 15')
+    # s = translate('01 02 03 04 05 06 07 08 09 10 11 12 13 00 14 15')
+    # s = translate('123456')
 
-# s = translate('01 02 03 04 05 06 08 14 09 11 07 00 13 10 15 12')
-# s = translate('01 02 03 04 05 06 07 08 09 10 11 12 13 00 14 15')
-# s = translate('01 02 03 04 05 06 07 08 09 10 11 12 13 00 14 15')
-# s = translate('123456')
+    Test_Case_1 = [[1, 2, 3, 4],[ 5, 6, 0, 8], [9, 10, 7, 12] , [13, 14, 11, 15]]
+    Test_Case_2 = [[1, 0, 3, 4],[ 5, 2, 7, 8], [9, 6, 10, 11] , [13, 14, 15, 12]]
+    Test_Case_3 = [[0, 2, 3, 4],[ 1,5, 7, 8], [9, 6, 11, 12] , [13, 10, 14, 15]]
+    Test_Case_4 = [[5, 1, 2, 3],[0,6, 7, 4], [9, 10, 11, 8] , [13, 14, 15, 12]]
+    Test_Case_5 = [[1, 6, 2, 3], [9,5, 7, 4], [0, 10, 11, 8] , [13, 14, 15, 12]]
 
-Test_Case_1 = [[1, 2, 3, 4],[ 5, 6, 0, 8], [9, 10, 7, 12] , [13, 14, 11, 15]]
-Test_Case_2 = [[1, 0, 3, 4],[ 5, 2, 7, 8], [9, 6, 10, 11] , [13, 14, 15, 12]]
-Test_Case_3 = [[0, 2, 3, 4],[ 1,5, 7, 8], [9, 6, 11, 12] , [13, 10, 14, 15]]
-Test_Case_4 = [[5, 1, 2, 3],[0,6, 7, 4], [9, 10, 11, 8] , [13, 14, 15, 12]]
-Test_Case_5 = [[1, 6, 2, 3], [9,5, 7, 4], [0, 10, 11, 8] , [13, 14, 15, 12]]
+    # Custom_Test_Case_1 = [[9,2,5,4],[13,11,0,3],[15,1,7,8],[10,6,14,12]]
+    s = translate_test_case(Test_Case_5) # start position
+    # s = translate_test_case(Custom_Test_Case_1) # start position
 
-# Custom_Test_Case_1 = [[9,2,5,4],[13,11,0,3],[15,1,7,8],[10,6,14,12]]
-s = translate_test_case(Test_Case_5) # start position
-# s = translate_test_case(Custom_Test_Case_1) # start position
+    solution = 'abcdefghijklmnoX' # desired solved position
 
-solution = 'abcdefghijklmnoX' # desired solved position
+    q = deque() # queue has O(1) pop time vs list's O(n)
+    q.append(s)
 
-q = deque() # queue has O(1) pop time vs list's O(n)
-q.append(s)
+    print(q)
 
-print(q)
-
-past_set = set() # store all previously checked nodes
-past_set.add(s)
-
-
-# Declare resulting state and corresponding action from a move (up to 4 possibilities) as None
-one = None; one_a =  None
-two = None; two_a =  None
-three = None; three_a =  None
-four = None; four_a =  None
-
-child_parent_dict = {} # keys are children, values are parents
-
-max_length_q = len(q)
-iterations = 0
-while(len(q) > 0):
-# while(iterations < 5000):
-
-    if q[0] == solution: # note: maybe consider reading q[0] to memory, since it's used later
-        print("Solved!")
-        break
-
-    # locate index of "0"
-    b = q[0].find('X') # blank index
+    past_set = set() # store all previously checked nodes
+    past_set.add(s)
 
 
-    if b == 0:
-        one = swap(q[0], 0, 1)
-        two = swap(q[0], 0, 4)
-        three = None
-        four = None
-    elif b == 1:
-        one = swap(q[0], 1, 0)
-        two = swap(q[0], 1, 2)
-        three = swap(q[0], 1, 5)
-        four = None
-    elif b == 2:
-        one = swap(q[0], 2, 1)
-        two = swap(q[0], 2, 3)
-        three = swap(q[0], 2, 6)
-        four = None
-    elif b == 3:
-        one = swap(q[0], 3, 2)
-        two = swap(q[0], 3, 7)
-        three = None
-        four = None
-    elif b == 4:
-        one = swap(q[0], 4, 0)
-        two = swap(q[0], 4, 5)
-        three = swap(q[0], 4, 8)
-        four = None
-    elif b == 5:
-        one = swap(q[0], 5, 1)
-        two = swap(q[0], 5, 4)
-        three = swap(q[0], 5, 6)
-        four = swap(q[0], 5, 9)
-    elif b == 6:
-        one = swap(q[0], 6, 2)
-        two = swap(q[0], 6, 5)
-        three = swap(q[0], 6, 7)
-        four = swap(q[0], 6, 10)
-    elif b == 7:
-        one = swap(q[0], 7, 3)
-        two = swap(q[0], 7, 6)
-        three = swap(q[0], 7, 11)
-        four = None
-    elif b == 8:
-        one = swap(q[0], 8, 4)
-        two = swap(q[0], 8, 9)
-        three = swap(q[0], 8, 12)
-        four = None
-    elif b == 9:
-        one = swap(q[0], 9, 5)
-        two = swap(q[0], 9, 8)
-        three = swap(q[0], 9, 10)
-        four = swap(q[0], 9, 13)
-    elif b == 10:
-        one = swap(q[0], 10, 6)
-        two = swap(q[0], 10, 9)
-        three = swap(q[0], 10, 11)
-        four = swap(q[0], 10, 14)
-    elif b == 11:
-        one = swap(q[0], 11, 7)
-        two = swap(q[0], 11, 10)
-        three = swap(q[0], 11, 15)
-        four = None
-    elif b == 12:
-        one = swap(q[0], 12, 8)
-        two = swap(q[0], 12, 13)
-        three = None
-        four = None
-    elif b == 13:
-        one = swap(q[0], 13, 9)
-        two = swap(q[0], 13, 12)
-        three = swap(q[0], 13, 14)
-        four = None
-    elif b == 14:
-        one = swap(q[0], 14, 10)
-        two = swap(q[0], 14, 13)
-        three = swap(q[0], 14, 15)
-        four = None
-    elif b == 15:
-        one = swap(q[0], 15, 11)
-        two = swap(q[0], 15, 14)
-        three = None
-        four = None
-    else:
-        one = None
-        two = None
-        three = None
-        four = None
+    # Declare resulting state and corresponding action from a move (up to 4 possibilities) as None
+    one = None; one_a =  None
+    two = None; two_a =  None
+    three = None; three_a =  None
+    four = None; four_a =  None
 
-    if one is not None and not one in q:
-        if not one in past_set:
-            q.append(one)
-            child_parent_dict[one] = q[0] # child is key, parent is current node
-    if two is not None and not two in q:
-        if not two in past_set:
-            q.append(two)
-            child_parent_dict[two] = q[0]
-    if three is  not None and not three in q:
-        if not three in past_set:
-            q.append(three)
-            child_parent_dict[three] = q[0]
-    if four is not None and not four in q:
-        if not four in past_set:
-            q.append(four)
-            child_parent_dict[four] = q[0]
-            
-    current_length_q = len(q)
-    if current_length_q >= max_length_q:
-        max_length_q = current_length_q
+    child_parent_dict = {} # keys are children, values are parents
+
+    max_length_q = len(q)
+    iterations = 0
+    while(len(q) > 0):
+    # while(iterations < 5000):
+
+        if q[0] == solution: # note: maybe consider reading q[0] to memory, since it's used later
+            print("Solved!")
+            break
+
+        # locate index of "0"
+        b = q[0].find('X') # blank index
 
 
-    # add to checked list (or set, in this case)
-    past_set.add(q[0])
-    q.popleft() # remove first element of queue (as we have checked that)
+        if b == 0:
+            one = swap(q[0], 0, 1)
+            two = swap(q[0], 0, 4)
+            three = None
+            four = None
+        elif b == 1:
+            one = swap(q[0], 1, 0)
+            two = swap(q[0], 1, 2)
+            three = swap(q[0], 1, 5)
+            four = None
+        elif b == 2:
+            one = swap(q[0], 2, 1)
+            two = swap(q[0], 2, 3)
+            three = swap(q[0], 2, 6)
+            four = None
+        elif b == 3:
+            one = swap(q[0], 3, 2)
+            two = swap(q[0], 3, 7)
+            three = None
+            four = None
+        elif b == 4:
+            one = swap(q[0], 4, 0)
+            two = swap(q[0], 4, 5)
+            three = swap(q[0], 4, 8)
+            four = None
+        elif b == 5:
+            one = swap(q[0], 5, 1)
+            two = swap(q[0], 5, 4)
+            three = swap(q[0], 5, 6)
+            four = swap(q[0], 5, 9)
+        elif b == 6:
+            one = swap(q[0], 6, 2)
+            two = swap(q[0], 6, 5)
+            three = swap(q[0], 6, 7)
+            four = swap(q[0], 6, 10)
+        elif b == 7:
+            one = swap(q[0], 7, 3)
+            two = swap(q[0], 7, 6)
+            three = swap(q[0], 7, 11)
+            four = None
+        elif b == 8:
+            one = swap(q[0], 8, 4)
+            two = swap(q[0], 8, 9)
+            three = swap(q[0], 8, 12)
+            four = None
+        elif b == 9:
+            one = swap(q[0], 9, 5)
+            two = swap(q[0], 9, 8)
+            three = swap(q[0], 9, 10)
+            four = swap(q[0], 9, 13)
+        elif b == 10:
+            one = swap(q[0], 10, 6)
+            two = swap(q[0], 10, 9)
+            three = swap(q[0], 10, 11)
+            four = swap(q[0], 10, 14)
+        elif b == 11:
+            one = swap(q[0], 11, 7)
+            two = swap(q[0], 11, 10)
+            three = swap(q[0], 11, 15)
+            four = None
+        elif b == 12:
+            one = swap(q[0], 12, 8)
+            two = swap(q[0], 12, 13)
+            three = None
+            four = None
+        elif b == 13:
+            one = swap(q[0], 13, 9)
+            two = swap(q[0], 13, 12)
+            three = swap(q[0], 13, 14)
+            four = None
+        elif b == 14:
+            one = swap(q[0], 14, 10)
+            two = swap(q[0], 14, 13)
+            three = swap(q[0], 14, 15)
+            four = None
+        elif b == 15:
+            one = swap(q[0], 15, 11)
+            two = swap(q[0], 15, 14)
+            three = None
+            four = None
+        else:
+            one = None
+            two = None
+            three = None
+            four = None
+
+        if one is not None and not one in q:
+            if not one in past_set:
+                q.append(one)
+                child_parent_dict[one] = q[0] # child is key, parent is current node
+        if two is not None and not two in q:
+            if not two in past_set:
+                q.append(two)
+                child_parent_dict[two] = q[0]
+        if three is  not None and not three in q:
+            if not three in past_set:
+                q.append(three)
+                child_parent_dict[three] = q[0]
+        if four is not None and not four in q:
+            if not four in past_set:
+                q.append(four)
+                child_parent_dict[four] = q[0]
+
+        current_length_q = len(q)
+        if current_length_q >= max_length_q:
+            max_length_q = current_length_q
 
 
-    # q
-    # print(q)
-    # print( str(iterations) + "----%.2f----" % (time.time() - st))
-    iterations += 1
+        # add to checked list (or set, in this case)
+        past_set.add(q[0])
+        q.popleft() # remove first element of queue (as we have checked that)
 
-print(past_set)
-print(iterations)
 
-soln_list = []
-current_node = q[0]
-# Build back up node solution path (from goal to start)
-while(1):
-    if current_node == s:
+        # q
+        # print(q)
+        # print( str(iterations) + "----%.2f----" % (time.time() - st))
+        iterations += 1
+
+    print(past_set)
+    print(iterations)
+
+    soln_list = []
+    current_node = q[0]
+    # Build back up node solution path (from goal to start)
+    while(1):
+        if current_node == s:
+            soln_list.append(current_node)
+            break
+
         soln_list.append(current_node)
-        break
+        parent_node = child_parent_dict[current_node]
+        current_node = parent_node
 
-    soln_list.append(current_node)
-    parent_node = child_parent_dict[current_node]
-    current_node = parent_node
+    soln_list.reverse() # reverse (to get start to goal)
 
-soln_list.reverse() # reverse (to get start to goal)
+    action_list = []
+    # Build back up action set list
+    for node in soln_list: # ignore first element (start pos) since it has no parent node
+        p1 = node.find('X')
+        try:
+            p2 = child_parent_dict[node].find('X')
+            action_list.append( position_to_actionset_dict.dual_moves_dict[(p1,p2)] )
+        except:
+            action_list.append('SSS')
 
-action_list = []
-# Build back up action set list
-for node in soln_list: # ignore first element (start pos) since it has no parent node
-    p1 = node.find('X')
-    try:
-        p2 = child_parent_dict[node].find('X')
-        action_list.append( position_to_actionset_dict.dual_moves_dict[(p1,p2)] )
-    except:
-        action_list.append('SSS')
+    print("length of set: ", len(past_set))
+    print(max_length_q)
+    pass
 
-print("length of set: ", len(past_set))
-print(max_length_q)
-pass
+if __name__ == '__main__':
+    main()
